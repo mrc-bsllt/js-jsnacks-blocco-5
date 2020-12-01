fdfd487361b61ca263d637888cde87ee2ce8abdc
MarcoB <mrc.bsllt@gmail.com>
2020-12-01 14:56:08 +0100

    completato versione ES6

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ $(document).ready(
   function() {
 
     // VERSIONE ES5-------------------------------------
-    var stringsArray = [
+    const stringsArray = [
       "primo",
       "secondo",
       "terzo",
@@ -16,23 +16,54 @@ $(document).ready(
       "nono",
       "decimo",
     ];
+    console.log(stringsArray);
 
-    var min = 2;
-    var max = 8;
+    let min = randomNumber(0, 9);
+    let max = itMustBeBigger(min);
+
+    console.log(
+      `
+      Valore minimo: ${min}
+      Valore massimo: ${max}
+      `
+    );
 
     console.log(arrayFilter(stringsArray, min, max));
 
   }
 );
 
-// FUNZIONI------------------------------------------
 
-// VERSIONE ES5-------------------------------------
-function arrayFilter(array, numMin, numMax) {
+// FUNZIONI VERSIONE ES5-------------------------------------
+
+// function arrayFilter(array, numMin, numMax) {
+//   var newArray = [];
+//
+//   for(var i = numMin; i < numMax; i++) {
+//     newArray.push(array[i]);
+//   }
+//   return newArray;
+// }
+
+// FUNZIONI VERSIONE ES6-------------------------------------
+
+const arrayFilter = (array, numMin, numMax) => {
   var newArray = [];
 
-  for(var i = numMin; i < numMax; i++) {
+  for(let i = numMin; i <= numMax; i++) {
     newArray.push(array[i]);
   }
   return newArray;
+};
+
+const randomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1) + min)
+
+const itMustBeBigger = (minValue) => {
+  var validNumber = randomNumber(0, 10);
+
+  while (validNumber <= minValue) {
+    validNumber = randomNumber(0, 10);
+  }
+
+  return validNumber;
 }
